bada191f5ba1027b5422be7d35f08421f2ed33e8
Kiwi <lazykiwi@live.com>
2012-07-13 13:54:10 +0800

    change name

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'怪叔叔奈特[怪叔叔也有梦想]',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
